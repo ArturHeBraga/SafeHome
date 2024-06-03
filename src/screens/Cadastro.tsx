c35207adbc0806ff5af42db31195555ec5f223f4
ArturHeBraga <artur.hebraga@outlook.com>
2024-06-03 12:13:35 -0300

    Código de cadastro funcionando

--- src/screens/Cadastro.tsx
+++ src/screens/Cadastro.tsx
@@ -1,25 +1,69 @@
+import React, { useState } from 'react';
 import { useFonts, BebasNeue_400Regular } from '@expo-google-fonts/bebas-neue';
-import { Text, View, Button, TextInput, TouchableOpacity } from 'react-native';
-import {styles} from '../styles/styles';
+import { Text, View, TextInput, TouchableOpacity, Alert } from 'react-native';
+import axios from 'axios';
+import { styles } from '../styles/styles';
 
-export default function Cadastro({navigation}: {navigation: any}) {
+export default function Cadastro({ navigation }: { navigation: any }) {
   const [fontLoaded] = useFonts({
     BebasNeue_400Regular
   });
+
+  const [nome, setNome] = useState('');
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
+
+  const handleCadastro = async () => {
+    try {
+      const response = await axios.post('https://localhost:5000/user', {
+        nome,
+        email,
+        senha,
+      });
+
+      if (response.status === 201) {
+        Alert.alert('Sucesso', 'Cadastro realizado com sucesso!');
+        navigation.navigate('Tab');
+      } else {
+        Alert.alert('Erro', 'Houve um problema ao realizar o cadastro.');
+      }
+    } catch (error) {
+      console.error(error);
+      Alert.alert('Erro', 'Houve um problema ao realizar o cadastro.');
+    }
+  };
+
   if (!fontLoaded) {
     return null;
   }
+
   return (
     <View style={styles.container}>
       <View style={styles.loginContainer}>
         <Text style={styles.titleLogin}>Cadastro</Text>
-        <TextInput style={styles.inputLogin} placeholder="Nome" />
-        <TextInput style={styles.inputLogin} placeholder="Email" />
-        <TextInput style={styles.inputLogin} placeholder="Senha" />
-        <TouchableOpacity style={styles.buttonLogin} onPress={() => {navigation.navigate('Tab')}}>
+        <TextInput
+          style={styles.inputLogin}
+          placeholder="Nome"
+          value={nome}
+          onChangeText={setNome}
+        />
+        <TextInput
+          style={styles.inputLogin}
+          placeholder="Email"
+          value={email}
+          onChangeText={setEmail}
+        />
+        <TextInput
+          style={styles.inputLogin}
+          placeholder="Senha"
+          secureTextEntry
+          value={senha}
+          onChangeText={setSenha}
+        />
+        <TouchableOpacity style={styles.buttonLogin} onPress={handleCadastro}>
           <Text>Cadastrar</Text>
         </TouchableOpacity>
       </View>
     </View>
-  )
+  );
 }
